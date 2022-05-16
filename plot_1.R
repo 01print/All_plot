2fa73f802d44473742d8bb0ff17056b2c63c1039
01pring <wulgre35pc@163.com>
2022-05-16 22:29:07 +0800

    Update plot_1.R

--- plot_1.R
+++ plot_1.R
@@ -6,3 +6,13 @@
 # xjtu
 #
 ##############
+
+## ConsensusClusterPlus // 一致性聚类
+## 列为样本，行为特征
+BiocManager::install("ConsensusClusterPlus")
+library(ConsensusClusterPlus)
+
+library(ALL)
+data(ALL)
+d=exprs(ALL) #提取表达数据
+d[1:5,1:5]
